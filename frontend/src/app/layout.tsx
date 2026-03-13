379907e709e3e936c7965202b0ea98931f7e2ed3
Nitanshu Tak <152681890+Nitanshu715@users.noreply.github.com>
2026-03-14 00:32:48 +0530

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Cormorant_Garamond } from "next/font/google";
+import "@fontsource/cormorant-garamond";
 import "./globals.css";
 
 const cormorant = Cormorant_Garamond({
